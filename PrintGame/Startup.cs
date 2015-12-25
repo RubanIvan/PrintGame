eecff30caff0a1f4ba066f2325056421d5da2a6b
Ruban Ivan <ivan@ruban.pro>
2015-12-25 15:22:16 +0300

    first

---
+++ PrintGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrintGame.Startup))]
+namespace PrintGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
