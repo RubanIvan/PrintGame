a6b5af4a5a88b8e4409ac2d9aa04ceeeef55ca71
Ruban Ivan <ivan@ruban.pro>
2016-02-03 17:17:21 +0300

    CMS

---
+++ CMS/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CMS
+{
+    /// <summary>Логика взаимодействия для MainWindow.xaml</summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Обрамление строк в параграф
+        private void ButtonTagP_Click(object sender, RoutedEventArgs e)
+        {
+            //Если не нашли открывающего тега
+            if (TextBoxDescript.Text.Contains("<p>") == false)
+            {
+                //то обрамляем строки в тег <p>..</p>
+                string[] TextList = TextBoxDescript.Text.Split(new[] {"\r\n", "\r", "\n"},
+                    StringSplitOptions.RemoveEmptyEntries);
+                TextBoxDescript.Text = "";
+                foreach (string t in TextList)
+                {
+                    TextBoxDescript.Text += "<p>" + t + "</p>\n";
+                }
+
+            }
+        }
+
+        //Создание списка
+        private void ButtonTagLi_Click(object sender, RoutedEventArgs e)
+        {
+            //Если не нашли открывающего тега
+            if (TextBoxComponents.Text.Contains("<li>") == false)
+            {
+                //то обрамляем строки в тег <li>..</li>
+                string[] TextList = TextBoxComponents.Text.Split(new[] {"\r\n", "\r", "\n"},
+                    StringSplitOptions.RemoveEmptyEntries);
+                TextBoxComponents.Text = "";
+                foreach (string t in TextList)
+                {
+                    TextBoxComponents.Text += "<li>" + t + "</li>\n";
+                }
+
+            }
+        }
+
+        //Проверка ввели ли правельно год
+        private void TextBoxYearPublishing_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxYearPublishing.Text == "") return;
+            int year = 0;
+            //Пробуем перевести в число
+            if (int.TryParse(TextBoxYearPublishing.Text, out year) == false)
+            {
+                //перевод не удался стираем неверное значение
+                TextBoxYearPublishing.Text = "";
+                return;
+            }
+            //если указан короткий год то дополняем его
+            if (year >= 70 && year <= 99) TextBoxYearPublishing.Text = (1900 + year).ToString();
+            if (year >= 0 && year <= 20) TextBoxYearPublishing.Text = (2000 + year).ToString();
+        }
+
+        //Тест коректно ли введен год
+        private bool TestYearPublishing()
+        {
+            int year = 0;
+            if (int.TryParse(TextBoxYearPublishing.Text, out year) == false)
+            {
+                TextBoxLogWindow.Text += "Неверно введен год\n";
+                return false;
+            }
+            //проверка попадания в диапозон
+            if (year < 1970 || year > 2020)
+            {
+                TextBoxLogWindow.Text += "Неверно введен год ошибка диапозона\n";
+                return false;
+            }
+            return true;
+        }
+
+        //Проверка на ввод рейтинга
+        private void TextBoxRating_LostFocus(object sender, RoutedEventArgs e)
+        {
+            float ratio;
+            if (float.TryParse(TextBoxRating.Text, out ratio) == false)
+            {
+                TextBoxRating.Text += "Неверно введен рейтинг\n";
+                return;
+            }
+            if (ratio < 0 || ratio > 10)
+            {
+                TextBoxRating.Text += "Неверно введен рейтинг выход за диапазон\n";
+                return;
+            }
+            TextBoxRating.Text = ratio.ToString("#.##");
+        }
+
+        //Тест коректно ли введен рейтинг
+        private bool TestRating()
+        {
+            float ratio;
+            if (float.TryParse(TextBoxRating.Text, out ratio) == false)
+            {
+                TextBoxRating.Text += "Неверно введен рейтинг\n";
+                return false;
+            }
+            if (ratio < 0 || ratio > 10)
+            {
+                TextBoxRating.Text += "Неверно введен рейтинг выход за диапазон\n";
+                return false;
+            }
+
+            TextBoxRating.Text = ratio.ToString("#.##");
+            return true;
+        }
+
+        //кол-во картинок геймплея
+        private void TextBoxImageGameplay_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TestImageGameplay();
+        }
+        //Тест картинок геймплея
+        private bool TestImageGameplay()
+        {
+            if (TextBoxImageGameplay.Text == "") return false;
+            int image = 0;
+            //Пробуем перевести в число
+            if (int.TryParse(TextBoxImageGameplay.Text, out image) == false)
+            {
+                //перевод не удался стираем неверное значение
+                TextBoxImageGameplay.Text = "";
+                TextBoxRating.Text += "Неверно введено кол-во картинок геймплея\n";
+                return false;
+            }
+
+            if (image < 0 || image > 6)
+            {
+                TextBoxImageGameplay.Text = "";
+                TextBoxRating.Text += "Неверно введено кол-во картинок геймплея выход за диапозон (0-5) \n";
+                return false;
+            }
+            return true;
+        }
+        //кол-во картинок примеры сканов
+        private void TextBoxImageScans_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TestImageScans();
+        }
+        //Тест картинок примеры сканов
+        private bool TestImageScans()
+        {
+            if (TextBoxImageScans.Text == "") return false;
+            int image = 0;
+            //Пробуем перевести в число
+            if (int.TryParse(TextBoxImageScans.Text, out image) == false)
+            {
+                //перевод не удался стираем неверное значение
+                TextBoxImageScans.Text = "";
+                TextBoxRating.Text += "Неверно введено кол-во картинок геймплея\n";
+                return false;
+            }
+
+            if (image <= 0 || image > 10)
+            {
+                TextBoxImageScans.Text = "";
+                TextBoxRating.Text += "Неверно введено кол-во картинок геймплея выход за диапозон (1-10) \n";
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ PrintGame/App_Data/print-game.sql
@@ -0,0 +1,21 @@
+--CREATE DATABASE PrintGameData;
+--DROP TABLE Game
+CREATE TABLE Game(
+	GameID			int IDENTITY NOT NULL PRIMARY KEY,	--идентификатор игры
+	TitleRu			nvarchar(1024) NOT NULL UNIQUE,		--Название по русски
+	TitleEn			nvarchar(1024) NOT NULL UNIQUE,		--Название по английски
+	CatalogName		nvarchar(1024) NOT NULL UNIQUE,		--Каталог с игрой
+	YearPublishing	int NOT NULL,						--Год издания
+	Rating			float(24) NOT NULL,					--Средний рейтинг
+	Lang			nvarchar(10),						--Язык игры
+	Descript		nvarchar(MAX) NOT NULL,				--Описание игры
+	NumOfPlayers	nvarchar(512),						--Количество игроков
+	NumOfSuggested	nvarchar(512),						--Рекомендуемое количество игроков
+	SuggestedAges	nvarchar(512),						--Рекомендуемый возраст игроков
+	Acquaintance	nvarchar(512),						--Время освоения игры
+	PlayingTime		nvarchar(512),						--Время партии
+	Components		nvarchar(MAX) NOT NULL,				--Состав коробки
+	ImageGameplay	int NOT NULL,						--Кол-во картинок с процессом игры
+	ImageScans		int NOT NULL,						--Кол-во картинок примеры сканов
+	CreateTime		SmallDateTime NOT NULL				--Время добавления 
+)
