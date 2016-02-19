8878e169d13836a5e9bfe1b8fe25e00eb97a47b7
Ruban Ivan <ivan@ruban.pro>
2016-02-19 17:27:30 +0300

    img galery 0.1

---
+++ Kostil1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kostil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintGameDataEntities enties =new PrintGameDataEntities();
+            //foreach (GameImage gameImage in enties.GameImage)
+            //{
+            //    gameImage.FulllImagePath=gameImage.FulllImagePath.Replace('\\', '/');
+            //    gameImage.SmallImagePath= gameImage.SmallImagePath.Replace('\\', '/');
+            //}
+            //enties.SaveChanges();
+            Console.ReadLine();
+        }
+    }
+}
