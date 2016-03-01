0c4b506211a856794da35a7a9e46acf10c36ad02
Ruban Ivan <ivan@ruban.pro>
2016-03-01 11:53:06 +0300

    user role

---
+++ PrintGame/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PrintGame.Controllers;
+using PrintGame.Models;
+
+namespace PrintGame.Areas.Admin.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        //отображаем доступные роли
+        public ActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        #region создать роль
+        // GET: /Roles/Create
+        // Создать новую роль 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: /Roles/Create
+        // Создать новую роль 
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+
+        #region изменить имя роли
+        // редактировать 
+        // GET: /Roles/Edit/
+        public ActionResult Edit(string roleName)
+        {
+            var thisRole = context.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            return View(thisRole);
+        }
+
+        // редактировать 
+        // POST: /Roles/Edit/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        #endregion
+
+        // удалить роль
+        // GET: /Roles/Delete/5
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            context.Roles.Remove(thisRole);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        #region назначение ролей
+        //вывести список назначенных ролей
+        public ActionResult ManageUserRoles()
+        {
+            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+
+        // назначить пользователю роль
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            if (userManager.IsInRole(user.Id, RoleName))
+            {
+                ViewBag.ResultMessage = "This user already has the role specified !";
+            }
+            else
+            {
+                userManager.AddToRole(user.Id, RoleName);
+                context.SaveChanges();
+
+                ViewBag.ResultMessage = "Username added to the role succesfully !";
+            }
+
+            //var account = new AccountController();
+            //account.UserManager.AddToRole(user.Id, RoleName);
+
+            //ViewBag.ResultMessage = "Role created successfully !";
+
+            // prepopulat roles for the view dropdown
+            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+
+            
+
+
+            return View("ManageUserRoles");
+        }
+
+
+        #endregion
+    }
+}
