6488f48e0b7a7acc6a9e02e2305bda37a8887c13
Ruban Ivan <ivan@ruban.pro>
2016-03-04 11:16:47 +0300

    end user manage

--- PrintGame/Areas/Admin/Controllers/UserController.cs
+++ PrintGame/Areas/Admin/Controllers/UserController.cs
@@ -8,11 +8,15 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using PrintGame.Models;
 
+
+
 namespace PrintGame.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
         // GET: Admin/User
+
+        [Authorize(Roles = "SuperAdmin")]
         public ActionResult Index()
         {
             return View();
@@ -27,19 +31,41 @@ namespace PrintGame.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        //[ValidateAntiForgeryToken]
         [Authorize(Roles = "SuperAdmin")]
         public JsonResult CreateUser(string email, string password)
         {
             ApplicationDbContext context = new ApplicationDbContext();
             var userStore = new UserStore<ApplicationUser>(context);
             var userManager = new UserManager<ApplicationUser>(userStore);
-            
 
-            var user = new ApplicationUser { UserName = email, Email = email,TwoFactorEnabled = false};
+            var user = new ApplicationUser { UserName = email, Email = email, TwoFactorEnabled = false,EmailConfirmed = true};
 
             IdentityResult result = userManager.Create(user, password);
-            
+
+            return Json(result);
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "SuperAdmin")]
+        public JsonResult DleteUser(string email)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            ApplicationUser user = userManager.FindByName(email);
+
+            IdentityResult result;
+            try
+            {
+                result = userManager.Delete(user);
+            }
+            catch (Exception e)
+            {
+                result = new IdentityResult(e.Message);
+            }
+
             return Json(result);
         }
 
